5c4feb3cd5e52ace52a557396dd0232a5f354cc5
burillu <burillu90@hotmail.it>
2023-11-09 00:02:16 +0100

    add component AppTitle

---
+++ src/components/AppTitle.vue
@@ -0,0 +1,22 @@
+<template>
+    <header>
+        <h1>{{ text }}</h1>
+    </header>
+</template>
+
+<script>
+export default {
+    name: 'AppTitle',
+    data() {
+        return {
+            text: 'La mia prima app con Vite!'
+        }
+    }
+}
+</script>
+
+<style scoped>
+h1 {
+    color: blue;
+}
+</style>
